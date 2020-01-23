8ad9a5f59db18d3e361f215308b4e70f095a7bbd
VIJAY FALOR <vijfalor@in.ibm.com>
2020-01-23 23:56:27 +0530

    Update HomeController.cs

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace app.Controllers
             return View();
         }
 
+	public IActionResult Categary1()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

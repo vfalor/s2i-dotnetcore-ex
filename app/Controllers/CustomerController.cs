8fa73b84e05ffa296dd32a862c52fa545d3ef706
vfalor <58453593+vfalor@users.noreply.github.com>
2020-02-04 23:51:56 +0530

    Add files via upload

---
+++ app/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Mvc;
+//using Microsoft.AspNetCore.Mvc;
+using app.Models;
+
+namespace app.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Details()
+        {
+		Customer customer = new Customer()
+		{
+			CustomerId = 101,
+			Name = "Rohan",
+			Gender = "Male",
+			City = "London"
+            	};
+		return View(customer);
+        }
+
+    }
+}

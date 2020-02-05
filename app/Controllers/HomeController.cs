c661fb1c2bf94b9e6f1933c1fbba60b35adc3752
vfalor <58453593+vfalor@users.noreply.github.com>
2020-02-05 19:00:15 +0530

    Add files via upload

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -6,13 +6,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using app.Models;
 
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
 namespace app.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+
+List<xxIBM_PRODUCT_STYLEModel> xxIBM_PRODUCT_STYLEs = new List<xxIBM_PRODUCT_STYLEModel>();
+        
+	string constr = "host=custom-mysql.gamification.svc.cluster.local; port=3306; database=sampledb";
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            string query = "SELECT ITEM_NUMBER, DESCRIPITION, LONG_DESCRIPTION FROM xxIBM_PRODUCT_STYLE";
+            using (MySqlCommand cmd = new MySqlCommand(query))
+            {
+               cmd.Connection = con;
+               con.Open();
+                using (MySqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        xxIBM_PRODUCT_STYLEs.Add(new xxIBM_PRODUCT_STYLE
+                        {
+                            ITEM_NUMBER = Convert.ToInt32(sdr["ITEM_NUMBER"]),
+                            DESCRIPITION = sdr["DESCRIPITION"].ToString(),
+                            LONG_DESCRIPTION = sdr["LONG_DESCRIPTION"].ToString()
+                        });
+                    }
+                }
+                con.Close();
+            }
+        }
+            return View(xxIBM_PRODUCT_STYLEs);
         }
 
         public IActionResult About()

c9187864a74274ee556dd515552f4a0a89263d68
vfalor <58453593+vfalor@users.noreply.github.com>
2020-02-05 18:28:28 +0530

    Add files via upload

--- app/Controllers/CustomerController.cs
+++ app/Controllers/CustomerController.cs
@@ -8,12 +8,43 @@ using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using app.Models;
 
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+
 namespace app.Controllers
 {
     public class CustomerController : Controller
     {
 	public ActionResult Index()
         {
+
+	List<xxIBM_PRODUCT_STYLE> xxIBM_PRODUCT_STYLEs = new List<xxIBM_PRODUCT_STYLE>();
+        //string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+	string constr = "host=custom-mysql.gamification.svc.cluster.local; port=3306; database=sampledb";
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            string query = "SELECT ITEM_NUMBER, DESCRIPITION, LONG_DESCRIPTION FROM xxIBM_PRODUCT_STYLE";
+            using (MySqlCommand cmd = new MySqlCommand(query))
+            {
+               cmd.Connection = con;
+               con.Open();
+                using (MySqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        xxIBM_PRODUCT_STYLEs.Add(new xxIBM_PRODUCT_STYLE
+                        {
+                            ITEM_NUMBER = Convert.ToInt32(sdr["ITEM_NUMBER"]),
+                            DESCRIPITION = sdr["DESCRIPITION"].ToString(),
+                            LONG_DESCRIPTION = sdr["LONG_DESCRIPTION"].ToString()
+                        });
+                    }
+                }
+                con.Close();
+            }
+        }
 		
 		//using (var context = new ApplicationDbContext())
 		//{
@@ -21,7 +52,7 @@ namespace app.Controllers
 		//List<xxIBM_PRODUCT_STYLE> IBM_PRODUCT_STYLEs = applicationDbContext.xxIBM_PRODUCT_STYLEs.ToList();
 
 
-		return View();
+		return View(xxIBM_PRODUCT_STYLEs);
 		//}
         }
 

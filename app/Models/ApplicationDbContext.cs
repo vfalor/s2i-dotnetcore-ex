4a240f0af521f231fe49bd720b9e0e3ce43c6d87
vfalor <58453593+vfalor@users.noreply.github.com>
2020-02-05 10:17:52 +0530

    Add files via upload

--- app/Models/ApplicationDbContext.cs
+++ app/Models/ApplicationDbContext.cs
@@ -10,9 +10,10 @@ namespace app.Models
     {
 	public DbSet<xxIBM_PRODUCT_STYLE> xxIBM_PRODUCT_STYLEs {get; set;}
 
-        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+        //public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)        : base(options)
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     	{
+        	optionsBuilder.UseSqlite("host=custom-mysql.gamification.svc.cluster.local; port=3306; database=sampledb; uid=xxuser; pwd=welcome1");
     	}
 
     	protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -38,7 +39,7 @@ namespace app.Models
 	public virtual DbSet<xxIBM_PRODUCT_SKU> XXIBM_PRODUCT_SKU {get; set;}
 	public virtual DbSet<xxIBM_PRODUCT_PRICING> XXIBM_PRODUCT_PRICING {get; set;}
 	public virtual DbSet<xxIBM_PRODUCT_CATALOGUE> XXIBM_PRODUCT_CATALOGUE {get; set;}
-	public virtual DbSet<xxIBM_PRODUCT_STYLE> XXIBM_PRODUCT_STYLE {get; set;}
+	//public virtual DbSet<xxIBM_PRODUCT_STYLE> XXIBM_PRODUCT_STYLE {get; set;}
 
 
     }

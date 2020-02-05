52354e7725672e8c4f22c029b747b323d69aad47
vfalor <58453593+vfalor@users.noreply.github.com>
2020-02-05 19:05:18 +0530

    Add files via upload

--- app/Models/xxIBM_PRODUCT_STYLEModel.cs
+++ app/Models/xxIBM_PRODUCT_STYLEModel.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace app.Models
 {

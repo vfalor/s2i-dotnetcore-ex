2f647420d8fe8121c827693d736a4ff306c83ed5
VIJAY FALOR <vijfalor@in.ibm.com>
2020-02-07 04:09:37 +0530

    Update PRODUCT_PRICING.cs

--- app/Models/PRODUCT_PRICING.cs
+++ app/Models/PRODUCT_PRICING.cs
@@ -10,8 +10,8 @@ namespace app.Models
         [Key]
 	public int PRICE_ID {get; set;}
 	public int ITEM_NUMBER {get; set;}
-  public float LIST_PRICE {get; set;}
-  public float DISCOUNT {get; set;}
+  public String LIST_PRICE {get; set;}
+  public String DISCOUNT {get; set;}
   public String IN_STOCK {get; set;}
   public int PRICE_EFFECTIVE_DATE {get; set;}
     }

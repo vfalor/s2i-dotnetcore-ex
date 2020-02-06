218eec875c5f96bfb4374ab1b8032c226ae4e943
VIJAY FALOR <vijfalor@in.ibm.com>
2020-02-07 03:50:32 +0530

    Create ProductPricingController.cs

---
+++ app/Controllers/ProductPricingController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+//using System.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using app.Models;
+
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+
+namespace app.Controllers
+{
+    public class PricingController : Controller
+    {
+	public ActionResult Index(string id)
+        {
+
+	List<PRODUCT_PRICING> pricingModels = new List<PRODUCT_PRICING>();
+        
+	string constr = "host=custom-mysql.gamification.svc.cluster.local; port=3306; database=sampledb; uid=xxuser; pwd=welcome1;";
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            string query = "SELECT * FROM XXIBM_PRODUCT_PRICING ";
+
+	    if(id != null)
+		query += " where LIST_PRICE like '%"+id+"%' or ITEM_NUMBER like '%"+id+"%' or DISCOUNT like '%"+id+"%' ";
+
+            using (MySqlCommand cmd = new MySqlCommand(query))
+            {
+               cmd.Connection = con;
+               con.Open();
+                using (MySqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        pricingModels.Add(new PRODUCT_PRICING
+                        {
+			    PRICEID = Convert.ToInt32(sdr["PRICEID"]),
+                            ITEM_NUMBER = Convert.ToInt32(sdr["ITEM_NUMBER"]),
+                            LIST_PRICE = Convert.ToFloat(sdr["LIST_PRICE"]),
+                            DISCOUNT = Convert.ToFloat(sdr["DISCOUNT"]),
+			    INSTOCK = sdr["INSTOCK"].ToString()
+
+                        });
+                    }
+                }
+                con.Close();
+            }
+        }
+		
+		
+
+	return View(pricingModels);	
+        }
+
+        
+    }
+}

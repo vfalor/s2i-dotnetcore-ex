ebab96bd02b70c99fbf61bec71bdd9311dbfc30c
VIJAY FALOR <vijfalor@in.ibm.com>
2020-02-07 02:02:48 +0530

    Update ProductController.cs

--- app/Controllers/ProductController.cs
+++ app/Controllers/ProductController.cs
@@ -25,9 +25,9 @@ namespace app.Controllers
 	string constr = "host=custom-mysql.gamification.svc.cluster.local; port=3306; database=sampledb; uid=xxuser; pwd=welcome1;";
         using (MySqlConnection con = new MySqlConnection(constr))
         {
-            //string query = "SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM XXIBM_PRODUCT_STYLE ";
+            string query = "SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM XXIBM_PRODUCT_STYLE ";
             //string query = "SELECT table_schema from information_schema.TABLES GROUP BY table_schema";
-	    string query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'base table' AND table_schema='sampledb' ";
+	    //string query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'base table' AND table_schema='sampledb' ";
 
 	    if(strSearch != null)
 		query += " where DESCRIPTION like '%"+strSearch+"%' ";
@@ -42,9 +42,9 @@ namespace app.Controllers
                     {
                         productModels.Add(new ProductModel
                         {
-                            ITEM_NUMBER = 1, //Convert.ToInt32(sdr["ITEM_NUMBER"]),
-                            DESCRIPTION = sdr["table_name"].ToString(),//sdr["DESCRIPTION"].ToString(),
-                            LONG_DESCRIPTION = sdr["table_name"].ToString()//sdr["LONG_DESCRIPTION"].ToString()
+                            ITEM_NUMBER = Convert.ToInt32(sdr["ITEM_NUMBER"]),
+                            DESCRIPTION = sdr["DESCRIPTION"].ToString(),
+                            LONG_DESCRIPTION = sdr["LONG_DESCRIPTION"].ToString()
                         });
                     }
                 }

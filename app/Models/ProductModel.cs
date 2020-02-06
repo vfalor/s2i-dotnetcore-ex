9a931b595ec527ad930535c5da8ad32eba67a080
VIJAY FALOR <vijfalor@in.ibm.com>
2020-02-06 10:41:37 +0530

    Create ProductModel.cs

---
+++ app/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app.Models
+{
+    public class ProductModel
+    {
+        [Key]
+	public int ITEM_NUMBER {get; set;}
+	public String DESCRIPITION {get; set;}
+	public String LONG_DESCRIPTION {get; set;}
+	public int CATALOGUE_CATEGORY {get; set;}
+	public String BRAND {get; set;}
+    }
+}
